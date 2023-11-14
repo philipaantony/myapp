001f75349556421a64d89e6ba7aa4ca9f54a1807
Philip Antony <128240465+philipaantony@users.noreply.github.com>
2023-11-14 11:54:18 +0530

    Update  view.js

---
+++ public/view.js
@@ -0,0 +1,17 @@
+// SampleComponent.jsx
+
+import React from 'react';
+
+// Define a functional component
+const SampleComponent = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a sample JSX file.</p>
+    </div>
+  );
+};
+
+export default SampleComponent;
+
+
